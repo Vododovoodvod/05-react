a2011001117eaa14ace2a4df4451996ec689126b
Vododovoodvod <tomislav.stasina@gmail.com>
2022-04-07 21:01:14 +0200

    jej

--- prva/src/App.js
+++ prva/src/App.js
@@ -1,56 +1,34 @@
-import logo from './image.jpg';
-import './App.css';
+import logo from "./image.jpg";
+import "./App.css";
 import { idHelpers } from "./library/helpers";
 import { RandomValue } from "./library/components/RandomValues";
 import { RepositoryLink } from "./library/components/RepositoryLink";
-import { ABQuestion } from "./library/components/ABQuestion";
-import { ABCQuestion } from "./library/components/ABCQuestion";
-import { FreeInputQuestion } from "./library/components/FreeInputQuestion";
+import { Quiz } from "./library/components/Quiz";
 import { useState } from "react";
 
-
 function App() {
-  const [state, setState] = useState({});
-
-  const handleAnswer = (id, choiceValue) => {
-    setState((currentState) => ({
-      ...currentState,
-      [id]: choiceValue,
-    }));
+  const [quizAnswer, setQuizAnswer] = useState(null);
+  const id = idHelpers.generateID();
+  const handleSubmit = (answers, id) => {
+    setQuizAnswer({ answers, id });
   };
-  
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>algebra-example-react-app</h1>
-        <p>Your unique ID is: {idHelpers.generateID()}</p>
+        <p>Your unique ID is: {id}</p>
         <RepositoryLink>View Repository</RepositoryLink>
         <RandomValue values={[4, 5, 6]} />
-        <div>
-          {state.question1}, {state.question2}, {state.question3}
-        </div>
-        <FreeInputQuestion id="question3" text="Enter your name" onKeyUp={handleAnswer} />
-        <ABQuestion
-          id="question1"
-          question="Make the right choice"
-          buttonA="Blue pill"
-          buttonB="Red pill"
-          buttonAValue="Blue"
-          buttonBValue="Red"
-          onChoice={handleAnswer}
-        />
-        <ABCQuestion
-          id="question2"
-          question="Make the right choice"
-          buttonA="Blue pill"
-          buttonB="Red pill"
-          buttonC="Pink pill"
-          buttonAValue="Blue"
-          buttonBValue="Red"
-          buttonCValue="Pink"
-          onChoice={handleAnswer}
-        />
+        {quizAnswer === null && <Quiz id={id} onSubmit={handleSubmit} />}
+        {quizAnswer !== null && (
+          <div>
+            <div>{quizAnswer.answers.question1}</div>
+            <div>{quizAnswer.answers.question2}</div>
+            <div>{quizAnswer.answers.question3}</div>
+          </div>
+        )}
       </header>
     </div>
   );
--- prva/src/library/components/Quiz.jsx
+++ prva/src/library/components/Quiz.jsx
@@ -1,3 +1,58 @@
-export function Quiz (props) {
-    
+import { useEffect, useState } from "react";
+import { ABCQuestion } from "./ABCQuestion";
+import { ABQuestion } from "./ABQuestion";
+import { FreeInputQuestion } from "./FreeInputQuestion";
+
+export function Quiz(props) {
+  const [state, setState] = useState({});
+
+    useEffect(() => {
+        console.log("First mount!");
+    }, []); //prazan array triggera sdamo prvi puta
+
+    useEffect(() => {
+        console.log("New state!");
+
+        return () => {
+            console.log("Old state!");
+        }
+    }) //ništa triggera uvijek
+
+  const handleAnswer = (id, choiceValue) => {
+    setState((currentState) => ({
+      ...currentState,
+      [id]: choiceValue,
+    }));
+  };
+
+  const handleSubmit = () => {
+    props.onSubmit(state, props.id);
+  };
+
+  return (
+    <div>
+      <FreeInputQuestion id="question3" text="Enter your name" onKeyUp={handleAnswer} />
+      <ABQuestion
+        id="question1"
+        question="Make the right choice"
+        buttonA="Blue pill"
+        buttonB="Red pill"
+        buttonAValue="Blue"
+        buttonBValue="Red"
+        onChoice={handleAnswer}
+      />
+      <ABCQuestion
+        id="question2"
+        question="Make the right choice"
+        buttonA="Blue pill"
+        buttonB="Red pill"
+        buttonC="Pink pill"
+        buttonAValue="Blue"
+        buttonBValue="Red"
+        buttonCValue="Pink"
+        onChoice={handleAnswer}
+      />
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
 }
