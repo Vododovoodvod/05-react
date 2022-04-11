b01bb269ed7c1e084f20727ad5b39e56627d7dcf
Vododovoodvod <tomislav.stasina@gmail.com>
2022-04-11 19:22:04 +0200

    update

--- prva/src/App.js
+++ prva/src/App.js
@@ -1,7 +1,5 @@
-import logo from "./image.jpg";
 import "./App.css";
 import { idHelpers } from "./library/helpers";
-import { RandomValue } from "./library/components/RandomValues";
 import { RepositoryLink } from "./library/components/RepositoryLink";
 import { Quiz } from "./library/components/Quiz";
 import { useState } from "react";
